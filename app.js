cb0e400353df6d8a989a185118c3fb0dd63b9684
Kenford <kzhou0066@gmail.com>
2018-08-14 02:09:12 -0400

    team player lists and team spymasters are updated among all connect clients

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ console.log('Server started!');
 var SOCKET_LIST = {};
 var data = {
 	spectators: [],
-	bluePlayers: [''],
-	redPlayers: ['']
+	bluePlayers: [],
+	redPlayers: [],
+	blueSpyMaster:'',
+	redSpyMaster:''
+};
+var buttonStates = {
+	blue: false,
+	red: false
 };
 
 /*socket.io setup
@@ -28,22 +34,61 @@ var data = {
 // io is an object that creates by the socket function
 var io = require('socket.io')(server,{});
 
-io.sockets.on('connection', function(socket){
-//	console.log('socket connection: '+ socket.id);
-	
+	io.sockets.on('connection', function(socket){
+	//	console.log('socket connection: '+ socket.id);
+		
 	//update a new player on the spectators list currently
-	socket.emit('newPlayer', data.spectators);
+	socket.emit('allSpectators', data.spectators);
+	socket.emit('allBluePlayers', data.bluePlayers);
+	socket.emit('allRedPlayers', data.redPlayers);
+	socket.emit('buttonStates', buttonStates);
+	socket.emit('nameOfBlueSpy', data.blueSpyMaster);
 
-socket.on('playerName', function(name){
-	data.spectators.push(name);
-	console.log(data.spectators);
-	io.sockets.emit('playerNames', name);
-})
-
-
-	socket.on('blue', function(){
-		console.log("Player has joined blue team: " + socket.id);
+	socket.on('playerName', function(name){
+		data.spectators.push(name);
+		console.log("spectators after entering: " + data.spectators);
+		io.sockets.emit('playerNames', name);
 	});
+
+	socket.on('blue', function(clientName){
+		console.log("Player: " + clientName + " has joined blue team");
+		data.bluePlayers.push(clientName);
+		io.sockets.emit('bluePlayer', clientName);
+		var spectatorIndex = data.spectators.indexOf(clientName);
+		data.spectators.splice(spectatorIndex, 1);
+		io.sockets.emit('removeSpectator', clientName);
+		console.log("spectators after joining blue team: " + data.spectators);
+	});
+
+	socket.on('red', function(clientName){
+		console.log("Player: " + clientName + " has joined red team");
+		data.redPlayers.push(clientName);
+		io.sockets.emit('redPlayer', clientName);
+		var spectatorIndex = data.spectators.indexOf(clientName);
+		data.spectators.splice(spectatorIndex, 1);
+		io.sockets.emit('removeSpectator', clientName);
+		console.log("spectators after joining red team: " + data.spectators);	
+	})
+
+	socket.on('blueSpy', function(nameOfSpyMaster){
+		io.sockets.emit('blueSpyButton', nameOfSpyMaster);
+		buttonStates.blue = true;
+		data.blueSpyMaster = nameOfSpyMaster;
+	})
+
+	socket.on('highlightBlueSpy', function(nameOfBlueSpy){
+		io.sockets.emit('highlightBlueSpy', nameOfBlueSpy);
+	})
+	
+	socket.on('redSpy', function(nameOfSpyMaster){
+		io.sockets.emit('redSpyButton', nameOfSpyMaster);
+		buttonStates.red = true;
+		data.redSpyMaster = nameOfSpyMaster;
+	})
+
+	socket.on('highlightRedSpy', function(nameOfRedSpy){
+		io.sockets.emit('highlightRedSpy', nameOfRedSpy);
+	})
 
 	//setInterval(function(){
 	//}1000/50);
--- client/client.js
+++ client/client.js
@@ -1,51 +1,203 @@
 window.onload=function(){
 
-const joinBlue_btn = document.querySelector(".join-blue");
-const joinRed_btn = document.querySelector(".join-red");
+const joinBlue_btn = document.querySelector("#blue");
+const joinRed_btn = document.querySelector("#red");
 const redSpy_btn = document.querySelector("#red-spy");
 const blueSpy_btn = document.querySelector("#blue-spy");
 const submit_name = document.querySelector("#name_btn");
 const name = document.querySelector("#name");
 
+var client = {
+	name: '',
+	team: '',
+	spymaster: false
+};
+var spectatorList = document.querySelector("#players");
+var bluePlayerList = document.querySelector("#blue-players");
+var redPlayerList = document.querySelector("#red-players");
+
+var wordType = {
+	redTeamStarts: ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'yellow', 'yellow' 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'black'],
+	blueTeamStarts: ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'yellow', 'yellow' 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'black'],
+}
 var socket = io();
 
 /* Function Definitions
 *************************************/
-function createName(data){
-	var playerList = document.querySelector("#players")
+function createSpectators(spectatorData){
+	createName(spectatorData, spectatorList);
+}
+
+function createBluePlayers(bluePlayerData){
+	createName(bluePlayerData, bluePlayerList);
+}
+
+function createRedPlayers(redPlayerData){
+	createName(redPlayerData, redPlayerList);
+}
+
+function createName(data, elementLocation){
 	var player = document.createElement("h3");
-	var node = document.createTextNode(data + ", ");
+	var node = document.createTextNode(data + "  ");
 	player.appendChild(node);
-	playerList.appendChild(player);
+	elementLocation.appendChild(player);
 	name.value = "";
 }
 
-function updateCurrentPlayers(data){
+function currentSpectators(allSpectators){
+	updateCurrentPlayers(allSpectators, spectatorList);
+}
+
+function currentBluePlayers(allBluePlayers){
+	updateCurrentPlayers(allBluePlayers, bluePlayerList);
+}
+
+function currentRedPlayers(allRedPlayers){
+	updateCurrentPlayers(allRedPlayers, redPlayerList);
+}
+
+function updateCurrentPlayers(data, elementLocation){
 	for(i=0;i<data.length;i++){
-		var playerList = document.querySelector("#players")
 		var player = document.createElement("h3");
-		var node = document.createTextNode(data[i] + ", ");
+		var node = document.createTextNode(data[i] + "  ");
 		player.appendChild(node);
-		playerList.appendChild(player);
+		elementLocation.appendChild(player);
+	}
+}
+
+function removeSpectator(spectator){
+	var childs = spectatorList.querySelectorAll("h3");
+	for(i=0;i<childs.length;i++){
+		if(childs[i].innerHTML == (spectator + '  '))
+			spectatorList.removeChild(childs[i]);
 	}
 }
 
 function sendNameToServer(){
 	console.log("the name is " + name.value);
 	socket.emit('playerName', name.value);
+	client.name = name.value;
+	console.log(client.name);
 }
 
 function joinBlueTeam(){
-	socket.emit('blue');
+	console.log(client.name);
+	socket.emit('blue', client.name);
+	client.team = 'blue';
 }
 
-socket.on('playerNames', createName);
-socket.on('newPlayer', updateCurrentPlayers);
+function joinRedTeam(){
+	socket.emit('red', client.name);
+	client.team = 'red';
+}
 
-/* MAIN
+function checkButtonStates(state){
+	if(state.blue == true)
+		removeBlueSpyButton();
+}
+
+function redSpyMaster(){
+	client.spymaster = true;
+	socket.emit('redSpy', client.name);
+}
+
+function blueSpyMaster(){
+	client.spymaster = true;
+	socket.emit('blueSpy', client.name);
+}
+
+function removeRedSpyButton(nameOfRedSpy){
+	var redSpy = document.querySelector("#red-spy-message");
+	var redSpyName = document.querySelector("#red-spy-name");
+	redSpyName.innerHTML = nameOfRedSpy;
+	if(nameOfRedSpy != ''){
+		redSpy_btn.style.display = "none";
+		redSpy.classList.remove("hide");
+	}
+	socket.emit('highlightRedSpy', nameOfRedSpy);
+}
+
+function removeBlueSpyButton(nameOfBlueSpy){
+	var blueSpy = document.querySelector("#blue-spy-message");
+	var blueSpyName = document.querySelector("#blue-spy-name");
+	blueSpyName.innerHTML = nameOfBlueSpy;
+	if(nameOfBlueSpy != ''){
+		blueSpy_btn.style.display = "none";
+		blueSpy.classList.remove("hide");
+	}
+	socket.emit('highlightBlueSpy', nameOfBlueSpy);
+}
+
+function highlightSpy(nameOfSpy){
+	var bluePlayers = bluePlayerList.querySelectorAll("h3");
+	var redPlayers = redPlayerList.querySelectorAll("h3");
+	var players;
+
+	console.log(client.team);
+
+	if(client.team == 'blue')
+		players = bluePlayers;
+	else if(client.team == 'red'){
+		players = redPlayers;
+		console.log(players);
+	}
+
+	console.log(bluePlayers);
+	console.log(redPlayers);
+	console.log(players);
+
+	if(client.team != ''){
+		for(i=0;i<players.length;i++){
+			console.log(nameOfSpy);
+			console.log(players[i].innerHTML);
+			if(players[i].innerHTML == (nameOfSpy + '  ')){
+				players[i].style.background = "grey";
+				players[i].style.border = "2px solid lightgrey";
+			}
+		}
+	}
+}
+/*
+function highlightSpy(nameOfSpy){
+	var bluePlayers = bluePlayerList.querySelectorAll("h3");
+	console.log(bluePlayers);
+	for(i=0;i<bluePlayers.length;i++){
+		console.log(nameOfSpy);
+		console.log(bluePlayers[i].innerHTML);
+		if(bluePlayers[i].innerHTML == (nameOfSpy + '  ')){
+			bluePlayers[i].style.background = "grey";
+			bluePlayers[i].style.border = "2px solid lightblue"
+		}
+	}
+}*/
+
+/* Sockets
+**************************************/
+socket.on('playerNames', createSpectators);
+// updating new players who joined later than others
+socket.on('allSpectators', currentSpectators);
+socket.on('allBluePlayers', currentBluePlayers);
+socket.on('allRedPlayers', currentRedPlayers);
+socket.on('buttonStates', checkButtonStates);
+socket.on('nameOfBlueSpy', removeBlueSpyButton);
+
+// move the clients' name to their respective teams
+socket.on('bluePlayer', createBluePlayers);
+socket.on('redPlayer', createRedPlayers);
+socket.on('removeSpectator', removeSpectator);
+
+// spy stuff
+socket.on('blueSpyButton', removeBlueSpyButton);
+socket.on('redSpyButton', removeRedSpyButton);
+socket.on('highlightBlueSpy', highlightSpy);
+socket.on('highlightRedSpy', highlightSpy)
+
+/* Event Listeners
 ***********************************/
 console.log(name_btn);
 submit_name.addEventListener("click", sendNameToServer);
 joinBlue_btn.addEventListener("click", joinBlueTeam);
-
+joinRed_btn.addEventListener("click", joinRedTeam);
+blueSpy_btn.addEventListener("click", blueSpyMaster);
+redSpy_btn.addEventListener("click", redSpyMaster);
 }
